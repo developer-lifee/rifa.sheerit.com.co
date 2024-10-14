2cae28f4b029e6b90008c8cf335da6f6c45b4137
Esteban Avila <eavila@uniempresarial.edu.co>
2024-10-13 19:01:42 -0500

    despliegue creacion de algunos componentes sin haberle pasado el js ni un html ya deberia ser capaz de usar el yaml para onstruir el proyecto

---
+++ frontend/my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Importa los estilos de Tailwind
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
